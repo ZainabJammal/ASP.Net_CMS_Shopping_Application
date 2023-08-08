53e6b1c73ba4ed51ddede6f6e42832248eea8b73
Zeinab <Zeinab@DESKTOP-M8HKRR3>
2023-08-08 22:28:07 +0300

    all

---
+++ CMS_Shopping_Cart/Models/Page.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS_Shopping_Cart.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Slug { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public int Sorting { get; set; }
+
+    }
+}
