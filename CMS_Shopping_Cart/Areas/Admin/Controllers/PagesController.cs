3890e7cfe77c365f95ba61f4a2ed72f9c6b24412
Zeinab <Zeinab@DESKTOP-M8HKRR3>
2023-08-09 21:12:47 +0300

    admin area

---
+++ CMS_Shopping_Cart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,64 @@
+﻿using CMS_Shopping_Cart.Infrastructure;
+using CMS_Shopping_Cart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS_Shopping_Cart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly CmsShoppingCartContext context;
+        public PagesController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        //GET /admin/pages
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
+        }
+
+        //GET /admin/pages/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if (page == null)
+                return NotFound();
+
+            return View(page);
+        }
+
+        //GET /admin/pages/create
+        public IActionResult Create() => View();
+
+        //POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Create(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists");
+                    return View(page);
+                }
+
+                context.Add(page);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
+        }
+    }
+}
