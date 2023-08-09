316dace82b27fbf805190b6f82669bce5246bf0c
Zeinab <Zeinab@DESKTOP-M8HKRR3>
2023-08-09 20:17:58 +0300

    first

---
+++ CMS_Shopping_Cart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMS_Shopping_Cart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
